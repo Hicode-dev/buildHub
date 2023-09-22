b21100f37801a4d307b61e5b910ddc9c575b2530
Hicode-dev <kingdavies2021@gmail.com>
2023-09-22 14:55:47 +0100

    qsw

--- src/app/Components/Navbar/Navbar.jsx
+++ src/app/Components/Navbar/Navbar.jsx
@@ -6,10 +6,7 @@ import {RiSunFill  ,
   RiMoonFill } from 'react-icons'
 
 const Navbar = () => {
-  function Navbar() {
-    const toggleDarkMode = () => {
-      document.querySelector('.navbar').classList.toggle('dark');
-    };
+
   return (
     <div className='w-full h-16 flex items-center justify-between pt-6 px-16'>
         <Image src={Logo} className='w-[9%]' alt='logo'/>
@@ -23,22 +20,12 @@ const Navbar = () => {
         <Link href='/subscribe' className='p-4'>Subscribe to premium</Link>
         <button className='mx-4 bg-black text-white rounded-3xl py-2 px-5'>Get Started</button>
       <div className='flex items-center w-[45px] border-gray-700 rounded-xl bg-gray-800 mx-4'>
-      <div className={darkMode ? 'dark' : 'light'}>
-      <div className="cursor-pointer text-gray-600">
-        {darkMode ? (
-          <RiSunFill   className="h-8 w-8 text-gray-600 dark:text-white" />
-        ) : (
-          <RiMoonFill  className="h-8 w-8 text-gray-600 dark:text-white" />
-        )}
-      </div>
-      {/* Your app content */}
-    </div>
+
 
       </div>
         </div>
     </div>
   )
-}
 }
 
 export default Navbar;
